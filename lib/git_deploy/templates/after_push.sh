2511de7bbd9595d4c007a8ee622618c37bce5acd
Koen Punt <koen@koenpunt.nl>
2013-07-14 15:23:40 +0200

    updated before/after scripts

--- lib/git_deploy/templates/after_push.sh
+++ lib/git_deploy/templates/after_push.sh
@@ -6,57 +6,13 @@ newrev=$(git rev-parse $2)
 refname="$3"
 tag=$(git describe --tags $refname)
 
-# IO redirection
-exec 6>&1           # Link file descriptor #6 with stdout. Saves stdout.
-exec > /dev/null    # All normal output to /dev/null
-
 run() {
-  [ -x $1 ] && $1 $oldrev $newrev $refname
+  [ -x $1 ] && $1 $oldrev $newrev $refname $tag
 }
-
-log() {
-  echo -e "----->   $@\n" >&6
-}
-
-against=$oldrev
-
-# prevent comparing to bad object
-if [ -z "${oldrev//0}" ]; then
-  against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
-fi
-
-echo >&6
-log "Publishing Fetch CMS $tag"
-
-log "Files changed between versions: ${oldrev:0:7}..${newrev:0:7}:\
- $(git diff $against $newrev --diff-filter=ACDMR --name-only | wc -l | tr -d ' ')"
 
 umask 002
 
 git submodule sync && git submodule update --init --recursive
 
-FRAMEWORK_DIR=/usr/share/php/fetch-cms-core
-
-. /usr/local/nvm/nvm.sh
-
-cd $FRAMEWORK_DIR/source
-
-composer --no-interaction install | sed 's/^/----->   /' >&6
-echo >&6
-
-nvm use | sed 's/^/----->   /' >&6
-echo >&6
-
-log "Installing npm packages"
-nvm use && npm --silent install
-
-log "Building assets using grunt"
-grunt --no-color release
-
-log "Syncing to $FRAMEWORK_DIR/$tag"
-rsync -lrpt --delete \
-  --exclude=".git" \
-  --exclude=".DS_Store" \
-  $FRAMEWORK_DIR/source/* $FRAMEWORK_DIR/$tag
-
-log "Done!"
+run deploy/before_restart
+run deploy/restart && run deploy/after_restart
--- lib/git_deploy/templates/before_restart.rb
+++ lib/git_deploy/templates/before_restart.rb
@@ -1,8 +1,54 @@
 #!/usr/bin/env ruby
-oldrev, newrev = ARGV
+require 'pty'
+
+oldrev, newrev, refname, tag = ARGV
+
+FRAMEWORK_DIR="/usr/share/php/fetch-cms-core"
 
 def run(cmd)
-  exit($?.exitstatus) unless system "umask 002 && #{cmd}"
+  begin
+    PTY.spawn( "umask 002 && #{cmd}" ) do |stdout, stdin, pid|
+      begin
+        # Do stuff with the output here. Just printing to show it works
+        stdout.each { |line| indent line.chomp }
+      rescue Errno::EIO
+      end
+    end
+  rescue PTY::ChildExited
+    exit($?.exitstatus)
+  end
 end
 
-RAILS_ENV   = ENV['RAILS_ENV'] || 'production'
+def log(string)
+  system "echo $'\e[1G'\"-----> #{string}\""
+end
+
+def indent(string)
+  system "echo $'\e[1G'\"       #{string}\""
+end
+
+system "echo '\e[1G'"
+log "Publishing Fetch CMS #{tag}"
+
+log "Installing dependencies with composer"
+run "composer --no-interaction install"
+indent "Dependencies installed"
+
+log "Resolving engine versions"
+node_version = File.read('.nvmrc').strip
+indent "Using Node.js version: #{node_version}"
+
+log "Installing dependencies with npm"
+run "/usr/local/nvm/#{node_version}/bin/npm install"
+indent "Dependencies installed"
+
+log "Compiling assets with grunt"
+run "grunt --no-color release 1> /dev/null"
+log "Assets compiled"
+
+log "Syncing to #{FRAMEWORK_DIR}/#{tag}"
+run "rsync -lrpt --delete --exclude='.git' #{FRAMEWORK_DIR}/source/ #{FRAMEWORK_DIR}/#{tag}"
+log "Done!"
+
+
+system "echo '\e[1G'"
